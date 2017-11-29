c2e26d2b4b631750e66bc9cbac45482a99b41574
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-11-28 22:47:07 -0500

    Getting Clsoe

--- Cluster/app.js
+++ Cluster/app.js
@@ -12,37 +12,9 @@ firebase.initializeApp(config);
 const preObject = document.getElementById('location');
 const dbRefObject = firebase.database().ref().child('location');
 var locations = []
+var newLocationLatTemp = 0
+var newLocationLngTemp = 0
 
-var hardcoded = [
-    {lat: -31.563910, lng: 147.154312},
-    {lat: -33.718234, lng: 150.363181},
-    {lat: -33.727111, lng: 150.371124},
-    {lat: -33.848588, lng: 151.209834},
-    {lat: -33.851702, lng: 151.216968},
-    {lat: -34.671264, lng: 150.863657},
-    {lat: -35.304724, lng: 148.662905},
-    {lat: -36.817685, lng: 175.699196},
-    {lat: -36.828611, lng: 175.790222},
-    {lat: -37.750000, lng: 145.116667},
-    {lat: -37.759859, lng: 145.128708},
-    {lat: -37.765015, lng: 145.133858},
-    {lat: -37.770104, lng: 145.143299},
-    {lat: -37.773700, lng: 145.145187},
-    {lat: -37.774785, lng: 145.137978},
-    {lat: -37.819616, lng: 144.968119},
-    {lat: -38.330766, lng: 144.695692},
-    {lat: -39.927193, lng: 175.053218},
-    {lat: -41.330162, lng: 174.865694},
-    {lat: -42.734358, lng: 147.439506},
-    {lat: -42.734358, lng: 147.501315},
-    {lat: -42.735258, lng: 147.438000},
-    {lat: -43.999792, lng: 170.463352}
-  ]
-
-//   console.log(hardcoded)
-   
-
-// THIS WORKS NOW
 dbRefObject.on('value', function(snapshot) {
 
     let returnArr = [];
@@ -60,8 +32,8 @@ dbRefObject.on('value', function(snapshot) {
 function initMap() {
 
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 3,
-        center: {lat: -28.024, lng: 140.887}
+        zoom: 7,
+        center: {lat: 27.3334823, lng: -81.5235359}
     });
 
     // Create an array of alphabetical characters used to label the markers.
@@ -81,5 +53,52 @@ function initMap() {
       // Add a marker clusterer to manage the markers.
     var markerCluster = new MarkerClusterer(map, markers,
         {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+
+    // For adding new addresses
+    var geocoder = new google.maps.Geocoder();
+        
+    document.getElementById('submit').addEventListener('click', function() {
+        geocodeAddress(geocoder, map);
+    });
+}
+
+
+function geocodeAddress(geocoder, resultsMap) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({'address': address}, function(results, status) {
+
+      if (status === 'OK') {
+        var latitude = results[0].geometry.location.lat()
+        var longitude = results[0].geometry.location.lng()
+
+        // This function runs twice for some reason
+        if (latitude === newLocationLatTemp && longitude === newLocationLngTemp) {
+            return;
+        }
+
+        dbRefObject.orderByChild("lng").equalTo(longitude).once("value",snapshot => {
+            const userData = snapshot.val();
+            // Dont duplicate places that exist - that's just wasteful 
+            if (userData){
+              alert("Oops! This location is already in the database. Please try another location.");
+              return;
+            }
+            else {
+                var newDBRefObject = dbRefObject.push();
+                newDBRefObject.set({
+                    lat: latitude, 
+                    lng: longitude
+                })
+            }
+        });
+        
+        newLocationLatTemp = latitude
+        newLocationLngTemp = longitude
+      } 
+      else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+
+    });
 }
 
